1c028f08c6b99209a2424ca4ea60a3654d2f508d
Milena <milenaalmeidadev@hotmail.com>
2021-05-11 20:17:43 -0400

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/Head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      
+      <h1>Hello world</h1>
+    </>
+  );
+}
